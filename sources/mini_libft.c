8ebf39371e95821fa9def9ba4ae8bfcd77e0d29e
RaphyStoll <raphael.ferreira@ik.me>
2025-05-08 01:21:35 +0200

    split de utils en minilibft

---
+++ sources/mini_libft.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini_libft.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/07 16:25:23 by raphaelferr       #+#    #+#             */
+/*   Updated: 2025/05/07 23:39:13 by raphaelferr      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+bool	is_digit(char *str)
+{
+	int i;
+
+	if (!str || str[0] == '\0')
+		return (false);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (false);
+		i++;
+	}
+	return (true);
+}
--- sources/utils.c
+++ sources/utils.c
@@ -1,17 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/07 15:01:39 by raphaelferr       #+#    #+#             */
+/*   Updated: 2025/05/07 16:25:55 by raphaelferr      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
+#include "colors.h"
 
-size_t	ft_strlen(const char *str)
-{
-	size_t i;
-
-	i = 0;
-	while(str[i])
-		i++;
-	return i;
-}
-
-int	error_msg(const char *msg)
+bool	error_msg(const char *msg)
 {
 	write(2, msg, ft_strlen(msg));
-	return (1);
+	write(2, "\n", 1);
+	return (false);
 }
