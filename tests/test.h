1cefba84acbe26b67eb225fae5855438baae84f6
RaphyStoll <raphael.ferreira@ik.me>
2025-05-08 09:53:00 +0200

    guard circulaire

--- tests/test.h
+++ tests/test.h
@@ -1,6 +1,13 @@
-#define RUN_SUBTEST(name, func) do { \
-	printf("----- %s -----", name); \
-	int result = func(); \
-	printf("result: %s\n", result ? "OK" : "KO"); \
-	failures += !result; \
-} while (0)
+#ifndef TEST_H
+# define TEST_H
+
+# include <stdio.h>
+
+# define RUN_SUBTEST(name, func) do { \
+		printf("----- %s -----\n", name); \
+		int result = func(); \
+		printf("result: %s\n\n", result ? "OK" : "KO"); \
+		failures += !result; \
+	} while (0)
+
+#endif
